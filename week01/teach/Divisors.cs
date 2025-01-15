f2055168b0ac39c1102e831ae1c11f5975371048
andreanat <andrea.nmb13@gmail.com>
2025-01-15 13:48:21 -0700

    divisors

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -18,7 +18,11 @@ public static class Divisors {
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new();
-        // TODO problem 1
+        for (int i=1; i <= number/2; i++) {//starts on one and goes up half a number
+            if (number % i == 0) {
+                results.Add(i);
+            }
+        }
         return results;
     }
 }
