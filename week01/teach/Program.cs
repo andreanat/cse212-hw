e3cc8702c3ef5bc98d5a7e051710a5f783dad8ef
andreanat <andrea.nmb13@gmail.com>
2025-01-16 07:58:44 -0700

    program

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -4,26 +4,11 @@ Divisors.Run();
 Console.WriteLine("\n======================\nArray Selector\n======================");
 ArraySelector.Run();
 
-// Uncomment this section when ready to test algorithm performance
 Console.WriteLine("\n======================\nAlgorithms\n======================");
 Algorithms.Run();
 
-// Uncomment these when ready to compare your implementations with solutions
 Console.WriteLine("\n======================\nDivisors Solution\n======================");
 DivisorsSolution.Run();
 
 Console.WriteLine("\n======================\nArray Selector Solution\n======================");
 ArraySelectorSolution.Run();
-
-// Uncomment when you are ready to run this
-// Console.WriteLine("\n======================\nAlgorithms\n======================");
-// Algorithms.Run();
-
-
-
-// Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors Solution\n======================");
-// DivisorsSolution.Run();
-//
-// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-// ArraySelectorSolution.Run();
