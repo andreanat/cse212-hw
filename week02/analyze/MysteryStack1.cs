ede2e38fcd8432b6845c47a86ba202cb269bbfd2
andreanat <andrea.nmb13@gmail.com>
2025-01-18 12:49:22 -0700

    update

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,5 +1,6 @@
 ﻿public static class MysteryStack1 {
-    public static string Run(string text) {
+    public static string Run(string text)
+     {
         var stack = new Stack<char>();
         foreach (var letter in text)
             stack.Push(letter);
@@ -10,4 +11,13 @@
 
         return result;
     }
+    class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(MysteryStack1.Run("racecar")); // Outputs: "racecar"
+        Console.WriteLine(MysteryStack1.Run("stressed")); // Outputs: "desserts"
+        Console.WriteLine(MysteryStack1.Run("a nut for a jar of tuna")); // Outputs: "anut fo raj a rof tun a"
+    }
+}
 }
