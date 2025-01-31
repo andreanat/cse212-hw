b042dd6743ac01098cd119682955f089185ce8d3
andreanat <andrea.nmb13@gmail.com>
2025-01-31 09:28:02 -0700

    changed wrong file

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -13,35 +13,23 @@ public class PersonQueue
     /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Add(person);//add the person to the end of the queue
+        _queue.Insert(0, person);
     }
-    /// <summary>
-    /// Remove a person from the queue
-    /// </summary>
-    /// <returns>The person that was removed</returns>
 
     public Person Dequeue()
     {
-        if (IsEmpty())
-        {
-            throw new InvalidOperationException("Queue is empty");
-        }
-        var person = _queue[0];//get the first person in the queue
-        _queue.RemoveAt(0);//remove the first person from the queue
+        var person = _queue[0];
+        _queue.RemoveAt(0);
         return person;
     }
-    // <summary>
-    /// Check if the queue is empty
-    /// </summary>
-    /// <returns>True if the queue is empty, false otherwise</returns>
 
     public bool IsEmpty()
     {
-        return Length == 0; //return true if the length of the queue is 0
+        return Length == 0;
     }
 
     public override string ToString()
     {
-        return $"[{string.Join(", ", _queue.Select(p => p.ToString()))}]";//joins each person to a single string
+        return $"[{string.Join(", ", _queue)}]";
     }
 }
